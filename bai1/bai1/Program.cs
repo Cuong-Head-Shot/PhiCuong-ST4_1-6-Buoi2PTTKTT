086bd8a965925b59e97c112c004544a95398cd89
NPC <cuongnguen150103@.com>
2024-07-17 14:21:58 +0700

    bai_buoi_2

---
+++ bai1/bai1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class BinarySearchAlgorithm
+{
+    public static int BinarySearch(int[] A, int n, int key)
+    {
+        int left = 0; 
+        int right = n - 1; 
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (A[mid] == key)
+            {
+                return mid; 
+            }
+            else if (key < A[mid])
+            {
+                right = mid - 1; 
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return -1; 
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] A = { 2, 4, 6, 8, 11, 12, 14, 16, 18, 20 };
+        int n = A.Length;
+        int key = 10;
+
+        int result = BinarySearch(A, n, key);
+
+        if (result != -1)
+        {
+            Console.WriteLine("Phan tu đuoc tim thay tai vi tri trong mang");
+        }
+        else
+        {
+            Console.WriteLine("Khong tim thay phan tu {key} trong mang");
+        }
+    }
+}
---
+++ bai2/bai2/bai2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+public class MergeSortAlgorithm
+{
+    
+    public static void MergeSort(int[] A, int left, int right)
+    {
+        if (left < right)
+        {
+            int mid = left + (right - left) / 2; 
+
+            MergeSort(A, left, mid);        
+            MergeSort(A, mid + 1, right);   
+
+            Merge(A, left, mid, right);     
+        }
+    }
+
+   
+    private static void Merge(int[] A, int left, int mid, int right)
+    {
+       
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+       
+        for (int i = 0; i < n1; i++)
+        {
+            L[i] = A[left + i];
+        }
+        for (int j = 0; j < n2; j++)
+        {
+            R[j] = A[mid + 1 + j];
+        }
+
+      
+        int p = 0;    
+        int q = 0;    
+        int k = left; 
+
+        while (p < n1 && q < n2)
+        {
+            if (L[p] <= R[q])
+            {
+                A[k] = L[p];
+                p++;
+            }
+            else
+            {
+                A[k] = R[q];
+                q++;
+            }
+            k++;
+        }
+
+        
+        while (p < n1)
+        {
+            A[k] = L[p];
+            p++;
+            k++;
+        }
+
+       
+        while (q < n2)
+        {
+            A[k] = R[q];
+            q++;
+            k++;
+        }
+    }
+
+    
+    public static void Main(string[] args)
+    {
+        int[] A = { 12, 11, 13, 5, 6, 7 };
+        int n = A.Length;
+
+        Console.WriteLine("Mảng ban đầu:");
+        PrintArray(A);
+
+        MergeSort(A, 0, n - 1);
+
+        Console.WriteLine("\nMảng sau khi sắp xếp Merge Sort:");
+        PrintArray(A);
+    }
+
+    
+    private static void PrintArray(int[] A)
+    {
+        foreach (var item in A)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
